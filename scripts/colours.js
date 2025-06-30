39a632bbfa8701386d6909b2ebc795f20a466f25
Jacob <jacobggray@outlook.com>
2025-06-30 07:27:46 +0100

    updated model bg

--- scripts/colours.js
+++ scripts/colours.js
@@ -12,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const b = +parts[2];
         const a = parts[3] !== undefined ? +parts[3] : 1;
 
+        root.style.setProperty('--primary-dark', `rgba(${Math.round(r * 2)}, ${Math.round(g * 2)}, ${Math.round(b * 2)}, ${a})`);
         root.style.setProperty('--primary-dark', `rgba(${Math.round(r * 0.5)}, ${Math.round(g * 0.5)}, ${Math.round(b * 0.5)}, ${a})`);
         root.style.setProperty('--primary-transparent', `rgba(${r}, ${g}, ${b}, 0.5)`);
     }
